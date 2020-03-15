c44c31f9e48e981b9174dbf5096158fb068b4ca1
Nikita <tariel-x@ya.ru>
2020-03-15 22:13:34 +0300

    map functions

--- src/lec1/maps/main.go
+++ src/lec1/maps/main.go
@@ -11,4 +11,19 @@ func main() {
 	fmt.Printf("%#v\n", stringsMap)
 	fmt.Printf("%#v\n", stringsMap["a"])
 	fmt.Printf("%#v\n", stringsMap["d"])
+
+	x := X{}
+	x.add("a", 1)
+	x.add("b", 2)
+	fmt.Printf("%#v\n", x)
+}
+
+type X map[string]int
+
+func (x *X) add(k string, v int) {
+	(*x)[k] = v
+}
+
+func (x X) addNoPtr(k string, v int) {
+	x[k] = v
 }
